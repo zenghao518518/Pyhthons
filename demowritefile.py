8f2492dbda079a9cdedfa4290bef9dca9e37b721
zenghao518518 <zenghao518518@163.com>
2023-06-17 20:54:43 +0800

    demowritefile

--- demowritefile.py
+++ demowritefile.py
@@ -3,8 +3,12 @@ import requests
 res = requests.get("http://www.10086.cn/support/selfservice/help/sh/5010801_4073_8801.json")
 data_dict = res.json()
 data_list = data_dict["cData"]['list']
+f = open("news.csv", mode='a', encoding='utf-8')
 for item in data_list:
     question = item['question']
     up_time = item['up_time']
     id = item['_orderId']
     print(up_time, id, question)
+    line = "{},{},{}\n".format(up_time, id, question)
+    f.write(line)
+f.close()
